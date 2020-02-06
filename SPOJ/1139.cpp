89fbf2dd457e97560431e4460ce4f892dcb95735
Radosław Myśliwiec <48944018+r-d-s-l-w@users.noreply.github.com>
2020-02-06 14:11:26 +0100

    Create 1139.cpp

---
+++ SPOJ/1139.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int d, x;
+int main()
+{
+    cin >> d;
+    for (int i=0; i<d; i++)
+    {
+        cin >> x;
+        cout << x*x << endl;
+    }
+    return 0;
+}

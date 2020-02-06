641b9f60c68e04bcb0ed71c87fb92b61dd034979
Radosław Myśliwiec <48944018+r-d-s-l-w@users.noreply.github.com>
2020-02-06 13:34:46 +0100

    Create 1111A - Superhero Transformation.cpp

---
+++ Codeforces/1111A - Superhero Transformation.cpp
@@ -0,0 +1,63 @@
+/// Radoslaw Mysliwiec 2019
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3")
+ 
+using namespace std;
+ 
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define ALL(x) (x).begin(),(x).end()
+#define endl '\n'
+#define DEBUG(x) cout << '>' << #x << ": " << x << endl;
+#define REP(i,n) for (int i = 1; i <= n; ++i)
+ 
+using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pi = pair<int,int>;
+using pll = pair<ll,ll>;
+using matrix = vector<vll>;
+ 
+constexpr ll MOD = 1000000007;
+constexpr ll INI = -1;
+constexpr ll IN = 350 + 10;
+constexpr ll INF = 1e9 + 1;
+constexpr ld EPS = 1e-9;
+ 
+inline ll two(ll n) {return 1<<n;}
+inline bool EQ(ld a, ld b) {return fabs(a-b)<EPS;}
+inline bool is_set(ll n, ll k) {return (n>>k)&1;}
+inline void set_bit(ll &n, ll k) {n|=two(k);}
+inline void unset_bit(ll &n, ll k) {n&=~two(k);}
+inline ll last_bit(ll n) {return n&(-n);}
+inline ll ones(ll n) {ll res=0;while(n&&++res)n-=last_bit(n);return res;}
+inline ll gcd(ll a, ll b) {return b==0?a:gcd(b,a%b);}
+inline ll lcm(ll a, ll b) {return a*(b/gcd(a,b));}
+ 
+string s, t;
+ 
+bool is_x(char c){
+    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') return 1;
+    return 0;
+}
+ 
+bool xd;
+ 
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cout.setf(ios::fixed); cout.precision(20);
+    //freopen("test.in","r",stdin); freopen("test.out","w",stdout);
+    cin >> s >> t;
+    if (s.length() != t.length()) cout << "No";
+    else {
+        for (int i=0; i<s.length(); ++i){
+            if (is_x(s[i]) != is_x(t[i])) xd = true;
+        }
+        if (xd) cout << "No";
+        else cout << "Yes";
+    }
+    return 0;
+}

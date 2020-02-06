cd9ef4c4b6a9b01b90f5d1ebf764434f71e18f5d
Radosław Myśliwiec <48944018+r-d-s-l-w@users.noreply.github.com>
2020-02-06 13:10:04 +0100

    Create 4A - Watermelon.cpp

---
+++ Codeforces/4A - Watermelon.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    int w;
+    cin >> w;
+    if (w%2 == 0 && w > 2) cout << "YES";
+    else cout << "NO";
+} 

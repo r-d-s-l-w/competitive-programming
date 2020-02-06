766a11245c43b7926bfce79a432c5b5d3323ffec
Radosław Myśliwiec <48944018+r-d-s-l-w@users.noreply.github.com>
2020-02-06 13:22:17 +0100

    Create 1093B - Letters Rearranging.cpp

---
+++ Codeforces/1093B - Letters Rearranging.cpp
@@ -0,0 +1,51 @@
+// Radek M
+/// while (clock()<=69*CLOCKS_PER_SEC)
+/// #pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("O3")
+/// #pragma GCC optimize("Ofast")
+/// #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+/// #pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+#define ALL(x) (x).begin(), (x).end()
+#define MOD 1000000007
+ 
+using namespace __gnu_pbds;
+using namespace std;
+ 
+using ll=long long;
+using ld = long double;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+using vi=vector<int>;
+using vll=vector<ll>;
+const int nax=1000*1007;
+const ll mod=1000*1000*1000+7;
+ 
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.setf(ios::fixed); cout.precision(20);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        bool xd = true;
+        cin >> s;
+        string k = s;
+        reverse(ALL(k));
+        if (s != k) cout << s << "\n";
+        else {
+            for (int i=0; i<s.length()-1; ++i){
+                if (s[i]!=s[i+1]) {
+                    swap(s[i],s[i+1]);
+                    xd = false;
+                    break;
+                }
+            }
+            if (xd) cout << -1 << endl;
+            else cout << s << endl;
+        }
+    }
+}

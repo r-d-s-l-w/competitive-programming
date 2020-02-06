48dff807ff5487876973326c676e3416369e87b2
Radosław Myśliwiec <48944018+r-d-s-l-w@users.noreply.github.com>
2020-02-06 15:36:44 +0100

    Create 1310.cpp

---
+++ SPOJ/1310.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string jednosci[10] =	{"","jeden","dwa","trzy","cztery","piec","szesc","siedem","osiem","dziewiec"};
+	string dziesiatki[10] =	{"","","dwadziescia","trzydziesci","czterdziesci","piecdziesiat","szescdziesiat","siedemdziesiat","osiemdziesiat","dziewiecdziesiat"};
+	string nastki[10] =	{"dziesiec","jedenascie","dwanascie","trzynascie","czternascie","pietnascie","szesnascie","siedemnascie","osiemnascie","dziewietnascie"};
+	string setki[10] =	{"","sto","dwiescie","trzysta","czterysta","piecset","szescset","siedemset","osiemset","dziewiecset"};
+	string skroty[5] =	{"","tys.","mln.","mld.","bln.",};
+	string liczba;
+	int t, i, x, cyfra;
+	cin >> t;
+	while (t--)
+	{
+		cin >> liczba;
+		x = liczba.length();
+		for (i = 0; i < x; ++i)
+		{
+			if ((x - i) % 3 == 0 && i != 0)
+				if (((i>0)?liczba[i-1]:'0') != '0' ||((i>1)?liczba[i-2]:'0') != '0' ||((i>2)?liczba[i-3]:'0') != '0')
+				{
+					cout << ((i!=0 && (x-i)/3!=0)?" ":"");
+					cout << skroty[(x - i) / 3];
+				}
+
+			switch ((x - i) % 3)
+			{
+			case 0:
+				cout << ((i!=0 && liczba[i]-'0'!=0)?" ":"");
+				cout << setki[liczba[i]-'0']; break;
+			case 2:
+				if (liczba[i] - '0' != 1)
+				{
+					cout << ((i!=0 && liczba[i]-'0'!=0 && liczba[i]-'0'!=1)?" ":"");
+					cout << dziesiatki[liczba[i]-'0'];
+				}
+				else
+					cout << ((i!=0)?" ":"") << nastki[liczba[i+1]-'0']; break;
+			case 1:
+				cout << ((i!=0 && liczba[i]-'0'!=0)?" ":"");
+				cout << jednosci[liczba[i]-'0']; break;
+			}
+
+			if ((x - i) % 3 == 2 && liczba[i] == '1')
+				++i;
+		}
+
+		cout << endl;
+	}
+
+	return 0;
+}

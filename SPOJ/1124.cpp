4f2d1da829a9fbb5afcef346b3df74dded6fdf87
Radosław Myśliwiec <48944018+r-d-s-l-w@users.noreply.github.com>
2020-02-06 20:38:49 +0100

    Create 1124.cpp

---
+++ SPOJ/1124.cpp
@@ -0,0 +1,14 @@
+import sys
+numbers = input()
+numbers = int(numbers)
+n=0
+while n<numbers:
+	n+=1
+	a, b = input().split()
+	a = int(a)
+	b = int(b)
+	if a // b * b == a:
+		print ("TAK")
+ 
+	else:
+		print ("NIE")
